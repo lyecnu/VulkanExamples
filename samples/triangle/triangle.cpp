bb56bdd3ffbb9998df6ade02385e900bca451e54
Kabutack <85118116@qq.com>
2025-12-10 01:32:41 +0800

    update triangle

--- samples/triangle/triangle.cpp
+++ samples/triangle/triangle.cpp
@@ -1,11 +1,62 @@
 #include "vulkanexamplebase.h"
 
+constexpr uint32_t MAX_CONCURRENT_FRAMES = 2;
+
 class VulkanExample : public VulkanExampleBase
 {
 public:
+	struct Vertex
+	{
+		float position[3];
+		float color[3];
+	};
+
+	struct
+	{
+		VkDeviceMemory memory{ VK_NULL_HANDLE };
+		VkBuffer buffer{ VK_NULL_HANDLE };
+	} vertices;
+
+	struct
+	{
+		VkDeviceMemory memory{ VK_NULL_HANDLE };
+		VkBuffer buffer{ VK_NULL_HANDLE };
+		uint32_t count{ 0 };
+	} indices;
+
+	struct UniformBuffer
+	{
+		VkDeviceMemory memory{ VK_NULL_HANDLE };
+		VkBuffer buffer{ VK_NULL_HANDLE };
+		VkDescriptorSet descriptorSet{ VK_NULL_HANDLE };
+		uint8_t* mapped{ nullptr };
+	};
+	std::array<UniformBuffer, MAX_CONCURRENT_FRAMES> uniformBuffers;
+	
+	struct ShaderData
+	{
+		glm::mat4 projectionMatrix;
+		glm::mat4 viewMatrix;
+		glm::mat4 modelMatrix;
+	};
+
+	VkPipelineLayout pipelineLayout{ VK_NULL_HANDLE };
+	VkPipeline pipeline{ VK_NULL_HANDLE };
+	VkDescriptorSetLayout descriptorSetLayout{ VK_NULL_HANDLE };
+
+	std::vector<VkSemaphore> presentCompleteSemaphores{};
+	std::vector<VkSemaphore> renderCompleteSemaphores{};
+
+	VkCommandPool commandPool{ VK_NULL_HANDLE };
+	std::array<VkCommandBuffer, MAX_CONCURRENT_FRAMES> commandBuffers{};
+	std::array<VkFence, MAX_CONCURRENT_FRAMES> waitFences{};
+
+	uint32_t currentFrame{ 0 };
+
+public:
 	VulkanExample() : VulkanExampleBase()
 	{
-		// todo
+		title = "Triangle";
 	}
 
 	~VulkanExample() override
@@ -15,9 +66,8 @@ public:
 
 	void prepare() override
 	{
-		// todo
-		VulkanExample::prepare();
-		//createSynchronizationPrimitives();
+		VulkanExampleBase::prepare();
+		createSynchronizationPrimitives();
 		//createCommandBuffers();
 		//createVertexBuffer();
 		//createUniformBuffers();
@@ -28,13 +78,33 @@ public:
 		prepared = true;
 	}
 
+	void createSynchronizationPrimitives()
+	{
+		for (uint32_t i = 0; i < MAX_CONCURRENT_FRAMES; i++)
+		{
+			VkFenceCreateInfo fenceCreateInfo
+			{
+				.sType = VK_STRUCTURE_TYPE_FENCE_CREATE_INFO,
+				.flags = VK_FENCE_CREATE_SIGNALED_BIT
+			};
+			VK_CHECK_RESULT(vkCreateFence(device, &fenceCreateInfo, nullptr, &waitFences[i]));
+		}
+
+		presentCompleteSemaphores
+	}
+
 	void render() override
 	{
-		std::cout << "Hello Triangle" << std::endl;
 		if (!prepared)
 		{
 			return;
 		}
+
+		VK_CHECK_RESULT(vkWaitForFences(device, 1, &waitFences[currentFrame], VK_TRUE, UINT64_MAX));
+		VK_CHECK_RESULT(vkResetFences(device, 1, &waitFences[currentFrame]));
+
+		uint32_t imageIndex;
+		vkAcquireNextImageKHR(device, swapChain.swapChain, UINT64_MAX, presentCompleteSemaphores, VK_NULL_HANDLE, imageIndex);
 	}
 
 };
